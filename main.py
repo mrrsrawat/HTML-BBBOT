b82cc88353c104b93b04c1ddef40b00afa25df90
rsrawatofficial <rsrasasingh@gmail.com>
2025-05-02 19:48:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -487,7 +487,7 @@ async def handle_file(client: Client, message: Message):
         f.write(html_content)
 
     # Send the HTML file to the user
-    await message.reply_document(document=html_file_path, caption="n\n\☑️ 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 𝐃𝐨𝐧𝐞!\n\n🔘 𝐄𝐱𝐭𝐫𝐚𝐜𝐭𝐞𝐝 𝐁𝐲 : 🚩 𝐉𝐀𝐈 𝐁𝐀𝐉𝐑𝐀𝐍𝐆 𝐁𝐀𝐋𝐈 🚩\n\n☑️ 𝐅𝐎𝐑 𝐀𝐍𝐘 𝐇𝐄𝐋𝐏 𝐂𝐎𝐍𝐓𝐀𝐂𝐓 𝐇𝐄𝐑𝐄 - [𝗝𝗔𝗜 𝗕𝗔𝗝𝗥𝗔𝗡𝗚 𝗕𝗔𝗟𝗜](http://t.me/krs_study_helper_bbot)")
+    await message.reply_document(document=html_file_path, caption="\n\n☑️ 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 𝐃𝐨𝐧𝐞!\n\n🔘 𝐄𝐱𝐭𝐫𝐚𝐜𝐭𝐞𝐝 𝐁𝐲 : 🚩 𝐉𝐀𝐈 𝐁𝐀𝐉𝐑𝐀𝐍𝐆 𝐁𝐀𝐋𝐈 🚩\n\n☑️ 𝐅𝐎𝐑 𝐀𝐍𝐘 𝐇𝐄𝐋𝐏 𝐂𝐎𝐍𝐓𝐀𝐂𝐓 𝐇𝐄𝐑𝐄 - [𝗝𝗔𝗜 𝗕𝗔𝗝𝗥𝗔𝗡𝗚 𝗕𝗔𝗟𝗜](http://t.me/krs_study_helper_bbot)")
 
     # Forward the .txt file to the channel
     await client.send_document(chat_id=CHANNEL_USERNAME, document=file_path)
