f9dcfb262956d66c6cd40208c06387463a59fa5e
mrrsrawat <rsrasasingh@gmail.com>
2025-06-03 22:04:31 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,15 @@
 # Don't Remove Credit Telegram - @mrkrsrawat
+import os
+from os import getenv
 from os import environ
 
 API_ID = int(environ.get("API_ID", "27900743"))
 API_HASH = environ.get("API_HASH", "ebb06ea8d41420e60b29140dcee902fc")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+OWNER_ID = int(os.environ.get("OWNER_ID", "7804396225"))
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://rsrasasingh:FS2G9YbI28KbPHLC@cluster0.yljr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+#auth_users = [7455272483 , 7804396225]
+# //LOG CHANNEL ID 
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002328169351"))
+ 
+SUDO_USERS = int(os.environ.get("SUDO_USERS", "7804396225"))
