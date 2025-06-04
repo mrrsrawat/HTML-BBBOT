259f6a54b24022001cff8f6acf2c82ec97aab0de
mrrsrawat <rsrasasingh@gmail.com>
2025-06-04 09:01:38 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -8,7 +8,7 @@ API_HASH = environ.get("API_HASH", "ebb06ea8d41420e60b29140dcee902fc")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER_ID = int(os.environ.get("OWNER_ID", "7804396225"))
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://rsrasasingh:FS2G9YbI28KbPHLC@cluster0.yljr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-auth_users = [7455272483 , 7804396225, 7969340586]
+auth_users = [7455272483 , 7804396225]
 # //LOG CHANNEL ID 
 #CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002328169351"))
  
