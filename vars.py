2ae5948f7a2138b0a076e4ff655f6ec7d337637f
mrrsrawat <rsrasasingh@gmail.com>
2025-06-07 18:01:40 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,10 +5,10 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "27900743"))
 API_HASH = environ.get("API_HASH", "ebb06ea8d41420e60b29140dcee902fc")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7945865890:AAGEvzgAhbyODr4jshy8yV5WNJ6UjU5-Zm4")
 OWNER_ID = int(os.environ.get("OWNER_ID", "7804396225"))
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://rsrasasingh:FS2G9YbI28KbPHLC@cluster0.yljr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-auth_users = [7455272483 , 7804396225]
+#MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://rsrasasingh:FS2G9YbI28KbPHLC@cluster0.yljr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+#auth_users = [7455272483 , 7804396225]
 # //LOG CHANNEL ID 
 #CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002328169351"))
  
