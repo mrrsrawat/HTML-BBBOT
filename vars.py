06b0353773d91da47cb4988da49a9bcaf8c806bd
mrrsrawat <rsrasasingh@gmail.com>
2025-06-07 18:45:45 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,12 +1,15 @@
 # Don't Remove Credit Telegram - @mrkrsrawat
 import os
 
+API_ID = int(os.environ.get("27900743"))
+API_HASH = os.environ.get("ebb06ea8d41420e60b29140dcee902fc")
+BOT_TOKEN = os.environ.get("7609528498:AAGqcl_h-Cygc4v_oiXqD4cbp-yGypVcakY")
+HTML_LOG_CHANNEL = int(os.environ.get("HTML_LOG_CHANNEL", "-1002328169351"))
 
-API_ID = 27900743
-API_HASH = "ebb06ea8d41420e60b29140dcee902fc"
-BOT_TOKEN = "7609528498:AAGqcl_h-Cygc4v_oiXqD4cbp-yGypVcakY"
+
+
 #OWNER_ID = int(os.environ.get("OWNER_ID", "7804396225"))
-HTML_LOG_CHANNEL = -1002328169351
+
 #MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://rsrasasingh:FS2G9YbI28KbPHLC@cluster0.yljr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 #auth_users = [7455272483 , 7804396225]
 # //LOG CHANNEL ID 
